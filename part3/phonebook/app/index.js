747ace0478979149ef1043309d38f353baded542
farid95g <faridg.1995@gmail.com>
2022-08-04 16:57:58 +0400

    phonebook backend exercises 3.7 and 3.8

--- part3/phonebook/app/index.js
+++ part3/phonebook/app/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 
 let persons = [
     { 
@@ -23,9 +24,23 @@ let persons = [
     }
 ]
 
+morgan.token('body', (req, res) => {
+    return JSON.stringify(req.body)
+})
+
 const app = express()
 
 app.use(express.json())
+app.use(morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      tokens.body(req, res)
+    ].join(' ')
+  }))
 
 app.get('/api/persons', (req, res) => {
     res.json(persons)
