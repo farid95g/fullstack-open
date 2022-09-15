b6b5b4ab7696b99b307129e52bba8a3cdd5d9f5d
farid95g <faridg.1995@gmail.com>
2022-09-15 15:52:07 +0400

    tests for list helper functions

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,9 +1,30 @@
 const dummy = (blogs) => {
-    console.log(blogs)
-    
-    return 1
+    return 1 ?? blogs.length
+}
+
+const totalLikes = (blogs) => {
+    const reducer = (total, blog) => total + blog.likes
+
+    return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs, prop) => {
+    const sortByProp = (a, b) => b[prop] - a[prop]
+
+    return !blogs.length 
+        ? {} 
+        : blogs
+            .sort(sortByProp)
+            .map(blog => ({
+                title: blog.title,
+                author: blog.author,
+                likes: blog.likes
+            }))[0]
+        
 }
 
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
 }
