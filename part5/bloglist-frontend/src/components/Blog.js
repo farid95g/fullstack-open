cd16eb910d64e5e63e1eefe1b34c1ec1bcd61741
farid95g <faridg.1995@gmail.com>
2022-10-20 16:03:21 +0400

    linter installed, configured, and errors fixed

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -2,58 +2,57 @@ import { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const Blog = ({ blog, updateBlog, removeBlog, userId }) => {
-  const [extended, setExtended] = useState(false)
+    const [extended, setExtended] = useState(false)
 
-  const show = { display: extended ? '' : 'none' }
-  const blogStyle = {
-    paddingTop: 10,
-    paddingLeft: 2,
-    border: 'solid',
-    borderWidth: 1,
-    marginBottom: 5
-  }
+    const show = { display: extended ? '' : 'none' }
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5,
+    }
 
-  return (
-    <div style={blogStyle}>
-      <div>
-        {blog.title} {blog.author}
-        <button onClick={() => setExtended(!extended)}>
-          { extended ? 'Hide' : 'View' }
-        </button>
-      </div>
-      <div style={show}>
-        {blog.url}<br />
-        likes {blog.likes}
-        <button
-          onClick={() => updateBlog(blog)}
-        >
-          Like
-        </button><br />
-        {blog.user.name}<br />
-        {
-          userId === blog.user.id && <button onClick={() => removeBlog(blog)}>Remove</button>
-        }
-      </div>
-    </div>
-  )
+    return (
+        <div style={blogStyle}>
+            <div>
+                {blog.title} {blog.author}
+                <button onClick={() => setExtended(!extended)}>
+                    {extended ? 'Hide' : 'View'}
+                </button>
+            </div>
+            <div style={show}>
+                {blog.url}
+                <br />
+                likes {blog.likes}
+                <button onClick={() => updateBlog(blog)}>Like</button>
+                <br />
+                {blog.user.name}
+                <br />
+                {userId === blog.user.id && (
+                    <button onClick={() => removeBlog(blog)}>Remove</button>
+                )}
+            </div>
+        </div>
+    )
 }
 
 Blog.propTypes = {
-  blog: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    author: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
-    likes: PropTypes.number.isRequired,
-    id: PropTypes.string.isRequired,
-    user: PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      username: PropTypes.string.isRequired
-    })
-  }),
-  updateBlog: PropTypes.func.isRequired,
-  removeBlog: PropTypes.func,
-  userId: PropTypes.string.isRequired
+    blog: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        author: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+        likes: PropTypes.number.isRequired,
+        id: PropTypes.string.isRequired,
+        user: PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired,
+            username: PropTypes.string.isRequired,
+        }),
+    }),
+    updateBlog: PropTypes.func.isRequired,
+    removeBlog: PropTypes.func,
+    userId: PropTypes.string.isRequired,
 }
 
 export default Blog
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -6,29 +6,29 @@ let token = ''
 const assignToken = jwt => token = jwt
 
 const addConfig = () => ({
-  headers: {
-    Authorization: `bearer ${token}`
-  }
+    headers: {
+        Authorization: `bearer ${token}`
+    }
 })
 
 const getAll = async () => {
-  const response = await axios.get(baseUrl, addConfig())
-  return response.data
+    const response = await axios.get(baseUrl, addConfig())
+    return response.data
 }
 
 const createNew = async (newBlog) => {
-  const response = await axios.post(baseUrl, newBlog, addConfig())
-  return response.data
+    const response = await axios.post(baseUrl, newBlog, addConfig())
+    return response.data
 }
 
 const updateBlog = async (updatedBlog) => {
-  const response = await axios.put(`${baseUrl}/${updatedBlog.id}`, updatedBlog, addConfig())
-  return response.data
+    const response = await axios.put(`${baseUrl}/${updatedBlog.id}`, updatedBlog, addConfig())
+    return response.data
 }
 
 const deleteBlog = async (blog) => {
-  const response = await axios.delete(`${baseUrl}/${blog.id}`, addConfig())
-  return response.status
+    const response = await axios.delete(`${baseUrl}/${blog.id}`, addConfig())
+    return response.status
 }
 
 export default { getAll, createNew, updateBlog, deleteBlog, assignToken }
